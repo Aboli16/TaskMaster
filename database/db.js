0f4a304fd87ac0c36d9684ae940a83f957529d31
Aboli <aboli.kulkarni2002@gmail.com>
2023-08-03 22:15:36 +0530

    last commit

--- database/db.js
+++ database/db.js
@@ -1,4 +1,27 @@
+
 const mongoose = require('mongoose');
+
+// Replace 'your-mongodb-atlas-connection-string' with the actual connection string from MongoDB Atlas
+const mongoURI = 'mongodb+srv://abolikulkarni2002:WWJ9034ggmQIQlEj@cluster0.zrq1vnw.mongodb.net/?retryWrites=true&w=majority';
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose
+  .connect(mongoURI, options)
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+    // Start your application or perform additional operations
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
+
+
+
+
+/*const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb://127.0.0.1:27017/task-manager';
 const options = {
@@ -15,3 +38,4 @@ mongoose
 	.catch((error) => {
 		console.error('Error connecting to MongoDB:', error);
 	});
+*/
